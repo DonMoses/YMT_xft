be5394bd9654a349ad8d7ca3b1b21f5711ad5dc7
Dan <DonMoses1989@gmail.com>
2015-04-02 16:32:18 +0800

    优化ListView适配器 （加入长按效果）

--- app/src/main/java/com/ymt/demo1/mainPages/HelpActivity.java
+++ app/src/main/java/com/ymt/demo1/mainPages/HelpActivity.java
@@ -1,24 +1,19 @@
 package com.ymt.demo1.mainPages;
 
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.ymt.demo1.R;
+import com.ymt.demo1.adapter.LongClickItemsAdapter;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 /**
  * Created by Moses on 2015
@@ -62,7 +57,7 @@ public class HelpActivity extends Activity {
 
         ListView questListView = (ListView) findViewById(R.id.common_quest_list_view);
         String[] quests = getResources().getStringArray(R.array.common_quest_array);
-        final CommonQuestAdapter questAdapter = new CommonQuestAdapter(this);
+        final LongClickItemsAdapter questAdapter = new LongClickItemsAdapter(this);
         questListView.setAdapter(questAdapter);
         ArrayList<String> mQuests = new ArrayList<>();
         Collections.addAll(mQuests, quests);
@@ -73,6 +68,10 @@ public class HelpActivity extends Activity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 questAdapter.setSelectedItemPosition(position);
                 questAdapter.notifyDataSetInvalidated();
+
+                //跳转到帮助详情页面
+
+
             }
         });
 
@@ -81,67 +80,17 @@ public class HelpActivity extends Activity {
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 questAdapter.setSelectedItemPosition(position);
                 questAdapter.notifyDataSetInvalidated();
+
+                //跳转到帮助详情页面
+
+
+
                 return true;
             }
         });
 
     }
 
-    protected class CommonQuestAdapter extends BaseAdapter {
-        List<String> mList = new ArrayList<>();
-        LayoutInflater inflater;
-        private int selectedPosition = -1;
 
-        public CommonQuestAdapter(Context context) {
-            inflater = LayoutInflater.from(context);
-        }
-
-        public void setmList(List<String> mList) {
-            this.mList = mList;
-            notifyDataSetChanged();
-        }
-
-        @Override
-        public int getCount() {
-            return mList.size();
-        }
-
-        public void setSelectedItemPosition(int position) {
-            selectedPosition = position;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return mList.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            TextView textView;
-            if (convertView == null) {
-                convertView = inflater.inflate(R.layout.item_view_common_quest, null);
-                textView = (TextView) convertView.findViewById(R.id.common_quest_item_view);
-                convertView.setTag(textView);
-            } else {
-                textView = (TextView) convertView.getTag();
-            }
-
-            textView.setText(mList.get(position));
-            if (selectedPosition != position) {
-                convertView.setBackgroundColor(Color.WHITE);
-                textView.setTextColor(Color.DKGRAY);
-            } else {
-                convertView.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_dark));
-                textView.setTextColor(Color.WHITE);
-            }
-
-            return convertView;
-        }
-    }
 
 }
