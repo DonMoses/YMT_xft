7aaf3144c322d489d569a29016c474bfc6352a10
Dan <DonMoses1989@gmail.com>
2015-04-19 09:32:10 +0800

    最近搜索  关键字类

---
+++ app/src/main/java/com/ymt/demo1/dbBeams/SearchString.java
@@ -0,0 +1,28 @@
+package com.ymt.demo1.dbBeams;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Dan on 2015/4/18
+ */
+
+public class SearchString extends DataSupport {
+    private int id;
+    private String searchedString;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSearchedString() {
+        return searchedString;
+    }
+
+    public void setSearchedString(String searchedString) {
+        this.searchedString = searchedString;
+    }
+}
