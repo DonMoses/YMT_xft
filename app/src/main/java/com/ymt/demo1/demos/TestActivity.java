8d562e9b99c0f50f19d61aeaa6c6f7e3591f216f
Dan <DonMoses1989@gmail.com>
2015-04-22 12:39:52 +0800

    keyboard and input

--- app/src/main/java/com/ymt/demo1/demos/TestActivity.java
+++ app/src/main/java/com/ymt/demo1/demos/TestActivity.java
@@ -1,8 +1,17 @@
 package com.ymt.demo1.demos;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.ymt.demo1.R;
@@ -14,10 +23,16 @@ import com.ymt.demo1.main.SearchActivity;
  */
 public class TestActivity extends Activity {
 
+    private InputMethodManager inputMethodManager;
+    protected enum INPUT_STYLE {
+        TEXT, TAP_VOICE, EMO, MORE_CATO
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         initTitle();
     }
 
@@ -44,10 +59,68 @@ public class TestActivity extends Activity {
             }
         });
 
-        /*
-        自定义View实现动画
-         */
+        final LinearLayout inputParent = (LinearLayout) findViewById(R.id.input_parent);
+        final EditText inputTxt = (EditText) findViewById(R.id.input_edit_text);
 
-
+        inputListener(inputParent, inputTxt);           //输入监听
     }
+
+    /**
+     * 输入框文字改变监听,动态设定外层布局高度
+     */
+    protected void inputListener(final LinearLayout parent, final EditText inputTxt) {
+
+         /*
+        editText文字改变监听
+         */
+        inputTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                //动态设置外层布局高度
+                parent.setLayoutParams(new FrameLayout.LayoutParams(
+                        FrameLayout.LayoutParams.WRAP_CONTENT,
+                        (int) (inputTxt.getHeight() + inputTxt.getTextSize()),
+                        Gravity.BOTTOM));
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    /**
+     * 点击监听
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        onHideSoftInput(event);
+        return super.onTouchEvent(event);
+    }
+
+    /**
+     * 点击空白处,关闭输入法软键盘
+     */
+    public void onHideSoftInput(MotionEvent event)
+    {
+        if (event.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            if (getCurrentFocus() != null && getCurrentFocus().getWindowToken() != null)
+            {
+                inputMethodManager.hideSoftInputFromWindow(
+                        getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            }
+        }
+    }
+
+
 }
